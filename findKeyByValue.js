b83c0f87293bbbba0c5ccfe0ba41c53541b9495b
archaemedes <wadecroft@gmail.com>
2021-04-28 22:46:23 +0000

    added a findKeyByValue function

---
+++ findKeyByValue.js
@@ -0,0 +1,25 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual  = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(object, key) {
+    for (const keys in object) {
+      if (key === object[keys]) {
+        return keys;
+      }
+    }
+};
+
+const favouriteMovies = {
+  sci_fi: "Star Wars",
+  horror: "The Shining",
+  comedy: "Knocked Up",
+  documentary: "Planet Earth"
+}
+
+console.log(findKeyByValue(favouriteMovies, "The Shining"));
