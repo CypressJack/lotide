ede374c4c82fab6badda3606c5c4d0024ff894f7
archaemedes <wadecroft@gmail.com>
2021-04-29 01:44:47 +0000

    Made eqObjects work :D

--- eqObjects.js
+++ eqObjects.js
@@ -7,23 +7,29 @@ const assertEqual  = function(actual, expected) {
 };
 
 const eqArrays = function(array1, array2) {
-    if (array1.join() === array2.join()) {
-      return true;
-    } else {
-      return false
-    }
-}
+  if (array1.join() === array2.join()) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
 const eqObjects = function(object1, object2) {
   let result = false;
   for (const key in object1) {
-    for (const keys in object2) {
-return result;
-}
-}
-}
+    if (object1[key] === object2[key]) {
+      result = true;
+    } else if ((Array.isArray(object1[key])) && (Array.isArray(object2[key]))) {
+      result = eqArrays(object1[key], object2[key]);
+    } else {
+      result = false;
+    }
+  }
+  return result;
+};
+
 
 const test1 = {
   a: 1,
@@ -39,21 +45,21 @@ const test2 = {
 };
 
 
-const cd = { 
+const cd = {
   c: "1",
   d: ["2", 3]
- };
+};
 
-const dc = { 
-  d: ["2", 3], 
+const dc = {
+  d: ["2", 3],
   c: "1"
- };
+};
 console.log(eqObjects(cd, dc)); // => true
 
 const cd2 = {
-  c: "1", 
+  c: "1",
   d: ["2", 3, 4]
- };
+};
 console.log(eqObjects(cd, cd2)); // => false
 
 const cd3 = {
